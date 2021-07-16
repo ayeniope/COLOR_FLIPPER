8e1f3f734f7cc49e57f732a9faf97e0282f58bcb
Ayeni opeyemi <ayeniopemoyin@gmail.com>
2021-07-16 21:11:29 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#This is a color flipper that displays hexadecimal colors and the usual primary/secondary colors   depending on the navigation link clicked.
+##technologies used
+
+- HTML5
+- CSS3
+- Javascript  
---
+++ hex.js
@@ -0,0 +1,15 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hexbtn = document.getElementById("hexbtn");
+const hexadcolor = document.querySelector(".hexadcolor");
+
+hexbtn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomNumber()];
+  }
+  hexadcolor.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+function getRandomNumber() {
+  return Math.floor(Math.random() * hex.length);
+}
